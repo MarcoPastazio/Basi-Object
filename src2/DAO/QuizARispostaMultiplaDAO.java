99aaa72fa6a3fffe6aade179f89d3ca9413f5227
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2022-08-23 17:07:03 +0200

    Delete src2/DAO directory

--- src2/DAO/QuizARispostaMultiplaDAO.java
+++
@@ -1,5 +0,0 @@
-package DAO;
-
-public interface QuizARispostaMultiplaDAO {
-	public boolean SalvaQuizMultipla(String nomedomanda, String descrizione, String domanda, float votorispostacorretta, float votorispostasbagliata, String nometest, String rispostacorretta);
-}
--- src2/DAO/RispostaApertaDAO.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-import java.util.ArrayList;
-
-public interface RispostaApertaDAO {
-	public String RicavoRisposta(String nometest, String nomestudente, int conto, int indice);
-	public boolean AggiornamentoRispostaAperta(ArrayList<String> DomandeTest, String nomestudente, int conto, float[] voti);
-}
