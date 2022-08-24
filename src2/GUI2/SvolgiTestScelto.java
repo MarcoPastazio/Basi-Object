88b18c337ca63ffde90b14eb253d69caddee4668
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2022-08-24 16:38:07 +0200

    Update SvolgiTestScelto.java

--- src2/GUI2/SvolgiTestScelto.java
+++ src2/GUI2/SvolgiTestScelto.java
@@ -50,7 +50,10 @@ public class SvolgiTestScelto extends JFrame {
     private JTextField textField;
 
 	/**
-	 * Launch the application.
+	 * serve a far svolgere il test scelto dallo studente
+	 * @param frameChiamante è il frame da cui viene chiamato
+         * @param r è l'oggetto che fa da intermediaro tra i vari package coinvolti
+         * @param s lo studente che sta usando l'applicazione
 	 */
     public SvolgiTestScelto(JFrame frameChiamante,Controller r, Studente s) {
     	this.frame=this;
@@ -58,31 +61,15 @@ public class SvolgiTestScelto extends JFrame {
     	getContentPane().setLayout(new BorderLayout());
     	//setDefaultCloseOperation(JPanel);
     	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    	//System.out.print("\n\n\n\nMAMAMAMAMAMAMAAMAMAMAMA\n"+t+"\n\n\n\n");
-	
-    	//this.dtm=t;
-    	//System.out.print("\ncicciolina\n"+t+" mammt a percr\n "+t==null+"\n\n\n");
+    	
 	
     	this.table=new JTable(r.buildTableModel(3,s,null));
-    	/*table.addAncestorListener(new AncestorListener() {
-		public void ancestorAdded(AncestorEvent event) {
-			table.setVisible(true);
-		}
-		public void ancestorMoved(AncestorEvent event) {}
-		public void ancestorRemoved(AncestorEvent event) {
-			table.setVisible(false);
-		}
-		});*/
-    	//System.out.print(table);
+    	
     	this.scrollPane=new JScrollPane(this.table);
     	this.vScroll=this.scrollPane.getVerticalScrollBar();
-    	//this.setLayout(new BorderLayout());
     	Dimension d = new Dimension(320, N_ROWS * table.getRowHeight());
     	table.setPreferredScrollableViewportSize(d);
-    	/*for (int i = 0; i < N_ROWS; i++) {
-    	System.out.println("\n\n\n\na soreta\n\n\n\n");
-        addRow();
-    	}*/
+    	
     	scrollPane.setVerticalScrollBarPolicy(
     			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
     	vScroll.addAdjustmentListener(new AdjustmentListener() {
@@ -150,16 +137,7 @@ public class SvolgiTestScelto extends JFrame {
     		}
     	});
     	mnNewMenu.add(btnNewButton_2);
-    /*JPanel panel = new JPanel();
-    panel.add(new JButton(new AbstractAction("Add Row") {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            addRow();//jooo
-        }
-    }));
-    this.add(panel, BorderLayout.SOUTH);*///baam
-
+   
 }
 }
 
