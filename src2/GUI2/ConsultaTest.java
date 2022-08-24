acd4684fcf263eba7ebb337fe6365484d47210f6
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2022-08-24 16:16:33 +0200

    Update ConsultaTest.java

--- src2/GUI2/ConsultaTest.java
+++ src2/GUI2/ConsultaTest.java
@@ -30,12 +30,10 @@ import javax.swing.JMenu;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.SystemColor;
-//import model.Studente;
 
 public class ConsultaTest extends JFrame {
 	
-	private static final int N_ROWS = 2;//modificato
-    //private static String[] header = {"Nome", "Corso", "Pubblicazione", "Durata","Insegnante"};
+	private static final int N_ROWS = 2;
     private DefaultTableModel dtm ;
     private JTable table ;
     private JScrollPane scrollPane ;
@@ -43,6 +41,13 @@ public class ConsultaTest extends JFrame {
     private int row;
     private JFrame frame;
     private boolean isAutoScroll;
+	
+     /**
+     * Permette di consultare i test allo studente.
+     * @param frameChiamante è il frame da cui viene chiamato
+     * @param st lo studente che sta usando l'applicazione
+     * @param r è l'oggetto che fa da intermediaro tra i vari package coinvolti
+     */
 
     public ConsultaTest(JFrame frameChiamante, Studente st,Controller r) {
     	this.frame=this;
@@ -50,31 +55,16 @@ public class ConsultaTest extends JFrame {
     	getContentPane().setLayout(new BorderLayout());
     	//setDefaultCloseOperation(JPanel);
     	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    	//System.out.print("\n\n\n\nMAMAMAMAMAMAMAAMAMAMAMA\n"+t+"\n\n\n\n");
     	
-    	//this.dtm=t;
-    	//System.out.print("\ncicciolina\n"+t+" mammt a percr\n "+t==null+"\n\n\n");
+    	
     	
     	this.table=new JTable(r.buildTableModel(2,st,null));
-    	/*table.addAncestorListener(new AncestorListener() {
-    		public void ancestorAdded(AncestorEvent event) {
-    			table.setVisible(true);
-    		}
-    		public void ancestorMoved(AncestorEvent event) {}
-    		public void ancestorRemoved(AncestorEvent event) {
-    			table.setVisible(false);
-    		}
-    	});*/
-    	//System.out.print(table);
+    	
     	this.scrollPane=new JScrollPane(this.table);
     	this.vScroll=this.scrollPane.getVerticalScrollBar();
-        //this.setLayout(new BorderLayout());
         Dimension d = new Dimension(320, N_ROWS * table.getRowHeight());
         table.setPreferredScrollableViewportSize(d);
-        /*for (int i = 0; i < N_ROWS; i++) {
-        	System.out.println("\n\n\n\na soreta\n\n\n\n");
-            addRow();
-        }*/
+       
         scrollPane.setVerticalScrollBarPolicy(
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         vScroll.addAdjustmentListener(new AdjustmentListener() {
@@ -113,28 +103,10 @@ public class ConsultaTest extends JFrame {
         	}
         });
         mnNewMenu.add(btnNewButton_2);
-        /*JPanel panel = new JPanel();
-        panel.add(new JButton(new AbstractAction("Add Row") {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                addRow();//jooo
-            }
-        }));
-        this.add(panel, BorderLayout.SOUTH);*///baam
+       
     }
 }
 
-    /*private void addRow() {//baam
-        char c = (char) ('A' + row++ % 26);
-        System.out.println("\n\ncess e mammt\n"+dtm+"\n\n");
-        dtm.addRow(new Object[]{
-                Character.valueOf(c),
-                String.valueOf(c) + String.valueOf(row),
-                Integer.valueOf(row),
-                Boolean.valueOf(row % 2 == 0)
-            });
-    }*/
-
+    
    
 
